2a8835f66435c81ac5b837b5b30ebc98cccbac55
wildy <me@koshaq.net>
2024-03-16 21:58:10 +0100

    Update modeminit.py

--- opt/scripts/modeminit.py
+++ opt/scripts/modeminit.py
@@ -7,10 +7,12 @@ try:
     time.sleep(0.5)
     phone.write(b'AT\r')
     time.sleep(0.5)
-    phone.write(b'AT+CFUN=1\r')
+    phone.write(b'AT+CFUN=1\r') # Turn the modem on
     time.sleep(10)
-    phone.write(b'AT+VOLTESETTING=1\r')
+    phone.write(b'AT+VOLTESETTING=1\r') # Enable VoLTE
     time.sleep(0.5)
-    phone.write(b'AT+CNV=/nv/item_files/modem/mmode/ue_usage_setting,0,01,1\r')
+    phone.write(b'AT+CNV=/nv/item_files/modem/mmode/ue_usage_setting,0,01,1\r') # Enable VoLTE
+    time.sleep(0.5)
+    phone.write(b'AT+CNMP=2\r') # Set network technology to 'All'
 finally:
     phone.close()
