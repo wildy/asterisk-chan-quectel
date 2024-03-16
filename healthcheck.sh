2a8e4f6c61bd2da679fe82302bee9157de32381d
wildy <me@koshaq.net>
2024-03-16 23:32:03 +0000

    Add a simple health check

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -euo pipefail
+if [ ! -c /dev/quectel0-data ] || [ ! -c /dev/quectel0-audio ]; then
+  echo "No Quectel devices detected; failing."
+  exit 1
+fi
+
 /usr/sbin/asterisk -rx "quectel show device state quectel0"
